48e81cd0a6d20cfe56b5b00ffa3ae0c08d3ffa6b
thepseudoatrist <immrishabh@gmail.com>
2018-06-02 12:51:30 +0530

    Partial Commit

--- app/src/main/java/nitrr/ecell/e_cell/RegisterMainActivity.java
+++ app/src/main/java/nitrr/ecell/e_cell/RegisterMainActivity.java
@@ -1,13 +1,13 @@
 package nitrr.ecell.e_cell;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register_main);
     }
 }
