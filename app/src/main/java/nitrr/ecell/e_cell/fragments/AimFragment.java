2df2f771cec1783c893049b55117e4b78e0dcf0d
thepseudoartist <immrishabh@gmail.com>
2018-07-25 23:20:20 +0530

    Aim Fragment Done.

--- app/src/main/java/nitrr/ecell/e_cell/fragments/AimFragment.java
+++ app/src/main/java/nitrr/ecell/e_cell/fragments/AimFragment.java
@@ -1,5 +1,6 @@
 package nitrr.ecell.e_cell.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import nitrr.ecell.e_cell.R;
 
 public class AimFragment extends Fragment {
+
+    private TextView aimText, aimContent, aimLabel, visionText, visionContent, visionLabel;
 
     public AimFragment(){}
 
@@ -28,5 +32,25 @@ public class AimFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        initialize();
+    }
+
+    private void initialize(){
+        Typeface raleway = Typeface.createFromAsset(getActivity().getAssets(), "fonts/raleway.ttf");
+        Typeface bebasNeue = Typeface.createFromAsset(getActivity().getAssets(), "fonts/BebasNeue.ttf");
+
+        aimText = getView().findViewById(R.id.aimText);
+        aimContent = getView().findViewById(R.id.aimContent);
+        aimLabel = getView().findViewById(R.id.aimLabel);
+
+        visionText = getView().findViewById(R.id.visionText);
+        visionContent = getView().findViewById(R.id.visionContent);
+        visionLabel = getView().findViewById(R.id.visionLabel);
+
+        aimLabel.setTypeface(bebasNeue);
+        visionLabel.setTypeface(bebasNeue);
+        aimText.setTypeface(bebasNeue);
+        visionText.setTypeface(bebasNeue);
     }
 }
